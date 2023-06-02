4c7aecfe8871cbe5344c6b5e5899cb46c8750407
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-06-02 10:56:44 -0700

    404 Component + Route + About component

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import './About.css';
+
+function About() {
+    return (
+        <div className="about">
+            <h1>About page</h1>
+            <p>Lorem Ipsum</p>
+        </div>
+    );
+}
+
+export default About;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import React, { useState } from 'react';
 
+import About from '../About/About';
 import BookDetails from '../BooksList/BookDetails/BookDetails';
 import BooksList from '../BooksList/BooksList';
+import NotFound from '../NotFound/NotFound';
 import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
-  const [search, setSearch] = useState('');
-  const [selectedBook, setSelectedBook] = useState(null);
+ 	const [search, setSearch] = useState('');
   
 	return (
 		<Router>
@@ -19,6 +20,11 @@ function App() {
 				<Routes>
 					<Route path="/" element={<BooksList search={search} />}></Route>
 					<Route path="/book/:bookId" element={<BookDetails />}></Route>
+
+					<Route path="/about" element={<About />} />
+
+					<Route path="*" element={<Navigate to="/404" />} />
+					<Route path="/404" element={<NotFound />} />
 				</Routes>
 			</div>
 		</Router>
