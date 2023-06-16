1c3334afb3817120ef1a33bd56d105c61aeec2ae
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-06-16 09:24:25 -0700

    FilterBar updates: validation + select/options

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -2,21 +2,46 @@ import React, { useContext } from "react";
 
 import { SearchContext } from "../../context";
 
+const languages = [
+	{ code: 'en', label: 'English' },
+	{ code: 'ru', label: 'Russian' },
+	{ code: 'fr', label: 'French' },
+	{ code: 'de', label: 'German' },
+];
+
 function FilterBar() {
     const { filters, setFilters } = useContext(SearchContext);
 
     const handleFilterChange = (event) => {
+        const { name, value } = event.target;
+
+        if (!value) {
+            alert('Please select a value');
+            return;
+        }
+
+        if (name === 'langRestrict' && !languages.some((lang) => lang.code === value)) {
+			alert('Selected language is not supported.');
+			return;
+		}
+
         setFilters({
             ...filters,
-            [event.target.name]: event.target.value,
+            [name]: value,
         });
     }
 
     return (
         <div className="filter-bar">
             <label>
-                Language: 
-                <input type="text" name="langRestrict" value={filters.langRestrict || ''} onChange={handleFilterChange} />
+                <select name="langRestrict" value={filters.langRestrict || ''} onChange={handleFilterChange}>
+                    <option value="">Select a language</option>
+                    {languages.map((lang) => (
+                        <option key={lang.code} value={lang.code}>
+                            {lang.label}
+                        </option>
+                    ))}
+                </select>
             </label>
         </div>
     )
